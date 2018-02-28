dcf8483b8f1f2ed0cf1f5e84a5259b6665360243
budparr <budparr@gmail.com>
2018-02-28 18:32:18 -0500

    Add new font

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-require("typeface-lato");
-import styles from "./../css/styles.css";
+require("typeface-vesper-libre")
+import styles from "./../css/styles.css"
 // import fonts from "./../fonts.css";
 
-import "./lazysizes.js";
-import "./turbolinks.js";
-import "./nojs.js";
+import "./lazysizes.js"
+import "./turbolinks.js"
+import "./nojs.js"
