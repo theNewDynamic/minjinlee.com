46b5672cadf0227527534671e02b89b2aa6c9133
budparr <budparr@gmail.com>
2018-02-28 18:31:55 -0500

    Clean up

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
+    require("postcss-import"),
     require("tailwindcss")("./src/css/tailwind-config.js"),
-    require('autoprefixer')
-  ]
-};
+    require("autoprefixer"),
+  ],
+}
