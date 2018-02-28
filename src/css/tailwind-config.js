3f13e591868bd7c3b8b7b477afb6a758b3f17140
budparr <budparr@gmail.com>
2018-02-28 18:34:22 -0500

    Rearrange stylesheets

--- src/css/tailwind-config.js
+++ src/css/tailwind-config.js
@@ -114,7 +114,7 @@ let colors = {
   blue: "#3490dc",
   "blue-light": "#6cb2eb",
   "blue-lighter": "#bcdefa",
-  "blue-lightest": "#eff8ff"
+  "blue-lightest": "#eff8ff",
   //
   // 'indigo-darkest': '#191e38',
   // 'indigo-darker': '#2f365f',
@@ -139,7 +139,7 @@ let colors = {
   // 'pink-light': '#fa7ea8',
   // 'pink-lighter': '#ffbbca',
   // 'pink-lightest': '#ffebef',
-};
+}
 
 module.exports = {
   /*
@@ -180,7 +180,7 @@ module.exports = {
     sm: "576px",
     md: "768px",
     lg: "992px",
-    xl: "1200px"
+    xl: "1200px",
   },
 
   /*
@@ -218,9 +218,10 @@ module.exports = {
       "Fira Sans",
       "Droid Sans",
       "Helvetica Neue",
-      "sans-serif"
+      "sans-serif",
     ],
     serif: [
+      "'Vesper Libre'",
       "Constantia",
       "Lucida Bright",
       "Lucidabright",
@@ -230,8 +231,8 @@ module.exports = {
       "Bitstream Vera Serif",
       "Liberation Serif",
       "Georgia",
-      "serif"
-    ]
+      "serif",
+    ],
     // ,
     // 'mono': [
     //   'Menlo',
@@ -271,7 +272,7 @@ module.exports = {
     "2xl": "1.5rem", // 24px
     "3xl": "1.875rem", // 30px
     "4xl": "2.25rem", // 36px
-    "5xl": "3rem" // 48px
+    "5xl": "3rem", // 48px
   },
 
   /*
@@ -296,7 +297,7 @@ module.exports = {
     medium: 500,
     semibold: 600,
     bold: 700,
-    extrabold: 800
+    extrabold: 800,
     // 'black': 900,
   },
 
@@ -315,7 +316,7 @@ module.exports = {
   leading: {
     none: 1,
     tight: 1.25,
-    normal: 1.5
+    normal: 1.5,
     // 'loose': 2,
   },
 
@@ -334,7 +335,7 @@ module.exports = {
   tracking: {
     tight: "-0.05em",
     normal: "0",
-    wide: "0.05em"
+    wide: "0.05em",
   },
 
   /*
@@ -385,7 +386,7 @@ module.exports = {
     "0": "0",
     "2": "2px",
     "4": "4px",
-    "8": "8px"
+    "8": "8px",
   },
 
   /*
@@ -428,7 +429,7 @@ module.exports = {
     sm: ".125rem",
     default: ".25rem",
     lg: ".5rem",
-    full: "9999px"
+    full: "9999px",
   },
 
   /*
@@ -479,7 +480,7 @@ module.exports = {
     "1/6": "16.66667%",
     "5/6": "83.33333%",
     full: "100%",
-    screen: "100vw"
+    screen: "100vw",
   },
 
   /*
@@ -514,7 +515,7 @@ module.exports = {
     "48": "12rem",
     "64": "16rem",
     full: "100%",
-    screen: "100vh"
+    screen: "100vh",
   },
 
   /*
@@ -533,7 +534,7 @@ module.exports = {
 
   minWidth: {
     "0": "0",
-    full: "100%"
+    full: "100%",
   },
 
   /*
@@ -553,7 +554,7 @@ module.exports = {
   minHeight: {
     "0": "0",
     full: "100%",
-    screen: "100vh"
+    screen: "100vh",
   },
 
   /*
@@ -581,7 +582,7 @@ module.exports = {
     "3xl": "80rem",
     "4xl": "90rem",
     "5xl": "100rem",
-    full: "100%"
+    full: "100%",
   },
 
   /*
@@ -600,7 +601,7 @@ module.exports = {
 
   maxHeight: {
     full: "100%",
-    screen: "100vh"
+    screen: "100vh",
   },
 
   /*
@@ -626,7 +627,8 @@ module.exports = {
     "3": "0.75rem",
     "4": "1rem",
     "6": "1.5rem",
-    "8": "2rem"
+    "8": "2rem",
+    "12": "8rem",
   },
 
   /*
@@ -653,7 +655,8 @@ module.exports = {
     "3": "0.75rem",
     "4": "1rem",
     "6": "1.5rem",
-    "8": "2rem"
+    "8": "2rem",
+    "12": "8rem",
   },
 
   /*
@@ -679,7 +682,7 @@ module.exports = {
     "3": "0.75rem",
     "4": "1rem",
     "6": "1.5rem",
-    "8": "2rem"
+    "8": "2rem",
   },
 
   /*
@@ -703,7 +706,7 @@ module.exports = {
     md: "0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)",
     lg: "0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)",
     inner: "inset 0 2px 4px 0 rgba(0,0,0,0.06)",
-    none: "none"
+    none: "none",
   },
 
   /*
@@ -726,7 +729,7 @@ module.exports = {
     "20": 20,
     "30": 30,
     "40": 40,
-    "50": 50
+    "50": 50,
   },
 
   /*
@@ -747,7 +750,7 @@ module.exports = {
     "25": ".25",
     "50": ".5",
     "75": ".75",
-    "100": "1"
+    "100": "1",
   },
 
   /*
@@ -765,7 +768,7 @@ module.exports = {
   */
 
   svgFill: {
-    current: "currentColor"
+    current: "currentColor",
   },
 
   /*
@@ -783,7 +786,7 @@ module.exports = {
   */
 
   svgStroke: {
-    current: "currentColor"
+    current: "currentColor",
   },
 
   /*
@@ -845,7 +848,7 @@ module.exports = {
     visibility: ["responsive"],
     whitespace: ["responsive"],
     width: ["responsive"],
-    zIndex: ["responsive"]
+    zIndex: ["responsive"],
   },
 
   /*
@@ -861,6 +864,6 @@ module.exports = {
   options: {
     prefix: "",
     important: false,
-    separator: ":"
-  }
-};
+    separator: ":",
+  },
+}
