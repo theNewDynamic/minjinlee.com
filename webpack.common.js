09c1282d47587c94acb8db4f87631e9c711cb4a3
budparr <budparr@gmail.com>
2018-03-13 21:43:11 -0400

    Update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,50 @@
 const path = require("path")
 const CleanWebpackPlugin = require("clean-webpack-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
   entry: {
     app: "./src/index.js",
+    // algolia: "./src/algolia.js"
   },
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+                minimize: true || {
+                  discardComments: {
+                    removeAll: true,
+                  },
+                  minifyFontValues: false,
+                  autoprefixer: false,
+                },
+              },
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                ident: "postcss",
+                plugins: () => [
+                  // require('postcss-flexbugs-fixes'),
+                  // autoprefixer({
+                  //   flexbox: 'no-2009',
+                  // }),
+                  require("postcss-import"),
+                  require("tailwindcss")("./src/css/tailwind-config.js"),
+                  require("autoprefixer"),
+                ],
+              },
+            },
+          ],
+        }),
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -24,11 +62,21 @@ module.exports = {
           {
             loader: "file-loader",
             options: {
+              // “publicPath” is used by several Webpack’s plugins to update the URLs inside CSS, HTML files when generating production builds.
               publicPath: "/dist/Fonts",
+              // “path” tells  Webpack where it should store the result
               outputPath: "/Fonts/",
             },
           },
         ],
+      },
+      {
+        //Mustache loader for Algolia templates
+        test: /\.html$/,
+        loader: "mustache-loader",
+        // loader: 'mustache-loader?minify'
+        // loader: 'mustache-loader?{ minify: { removeComments: false } }'
+        // loader: 'mustache-loader?noShortcut'
       },
     ],
   },
